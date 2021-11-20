8f87a6dc7770b0086d6d2f6e1535033fe32fe658
xiangyongjun <icecore-pdt@outlook.com>
2021-11-20 15:20:54 +0800

    修复 cdn 失效切换本地依赖失败的 bug

--- README.md
+++ README.md
@@ -4,8 +4,6 @@
 
 > 一个可以将 `node_module` 依赖替换成 `cdn` 资源的 `webpack` 插件，而且支持 `cdn` 失效时自动切换到本地备份的依赖。
 
-中文 | [English](https://github.com/xiangyongjun/auto-webpack-cdn-plugin/blob/main/README.EN.md)
-
 ## 简介
 `auto-webpack-cdn-plugin` 只需要简单配置，即可以将项目中使用到的依赖资源替换成 cdn 资源，并且支持 cdn 依赖备份，打包的时候自动下载 cdn 上对应版本的资源进行本地备份，当 cdn 失效时会自动切换成本地的依赖，解决小水管服务器用户的痛点。
 
@@ -40,11 +38,13 @@ module.exports = {
           { from: 'vue-router', to: 'VueRouter', path: 'dist/vue-router.min.js' },
           { from: 'vuex', to: 'Vuex', path: 'dist/vuex.min.js' },
           { from: 'axios', to: 'axios', path: 'dist/axios.min.js' },
-          { from: 'element-ui', to: 'Element', path: 'lib/index.js' },
-          { from: 'moment', to: 'moment', path: 'min/moment.min.js', final: true },
+          { from: 'element-ui', to: 'ELEMENT', path: 'lib/index.js' },
           { from: 'js-cookie', to: 'Cookies', path: 'dist/js.cookie.min.js' },
           { from: 'nprogress', to: 'NProgress', path: 'nprogress.js' },
+          { from: 'moment', to: 'moment', path: 'min/moment.min.js', final: true },
           { from: 'element-ui', path: 'lib/theme-chalk/index.css' },
+          { from: 'element-ui', path: 'lib/theme-chalk/fonts/element-icons.ttf' },
+          { from: 'element-ui', path: 'lib/theme-chalk/fonts/element-icons.woff' },
           { from: 'normalize.css', path: 'normalize.css' },
         ],
         // from（模块名称），to（模块导出的全局对象名称，css可略），path（资源路径，可填入完整 url），final（首屏渲染后加载，css可略）
@@ -67,29 +67,7 @@ if (!window.Vuex) Vue.use(VueRouter);
 // 当需要被替换的依赖被 Vue.use() 时，需要手动加上判断语句 if(!window.模块导出的全局对象名称) ，否则运行将会报错
 ```
 
-## 实现原理
-1. 提取 `html` 模板的 `body` 代码，注入 `<script cdn></script>` 代码。
-2. 注入 `script` 、 `link` 标签（绑定了 `onload` 、 `onerror` 事件）到 `html` 模板。
-3. 当 `webpack` 打包时下载相应的 `cdn` 资源到本地的 `backup` 目录。
-4. 当标签的 `onerror` 事件被执行时，`src` 、 `href` 将会替换成本地的资源。
-5. 当标签的 `onload` 事件被执行时，计数器自增 1 ，当计数器等于注入的 `script` 数量时，将提取的 `body` 的代码写入到 `<body></body>` ，此时首屏内容开始加载，带有 `final` 标记的 `script` 标签会在首屏渲染后加载，最后清除所有 `onload` 、 `onerror` 、 `backup` 、 `final` 等标签属性，以及清除 `<script cdn></script>`。
-```html
-<!-- 用于 cdn 失效时切换本地资源的代码，所有注入的 script 加载完毕后会自行移除 -->
-<script cdn>/* ... */</script>
-
-<!-- 标签加载完成后，计数器自增 1 -->
-<script onload="loadSuccess.call(this)"></script>
-
-<!-- 标签加载失败后，src 、 href 将会替换成本地的资源 -->
-<script onerror="loadCSS(this)"></script>
-
-<!-- 用于记录本地资源地址 -->
-<script backup="./backup/xxx"></script>
-
-<!-- 首屏渲染后加载 -->
-<script final></script>
-```
-
+## 执行流程
 注入 CDN 后的 HTML 模板
 ```html
 <!DOCTYPE html>
@@ -98,18 +76,18 @@ if (!window.Vuex) Vue.use(VueRouter);
     <meta charset="UTF-8">
     <title>动态 CDN</title>
     <!-- 以下是注入的代码 -->
-    <script cdn>/* 用于 cdn 失效时切换本地资源的代码，所有注入的 script 加载完毕后会自行移除 */</script>
-    <link rel="stylesheet" onload="loadSuccess.call(this)" backup="./backup/element-ui@2.15.6/index.css" onerror="loadCSS(this)" href="https://unpkg.zhimg.com/element-ui@2.15.6/lib/theme-chalk/index.css">
-    <script type="text/javascript" crossorigin="anonymous" backup="./backup/vue@2.6.10/vue.min.js" onload="loadSuccess.call(this)" onerror="loadScript(this)" src="https://unpkg.zhimg.com/vue@2.6.10/dist/vue.min.js"></script>
+    <script cdn>/* 用于 cdn 失效时切换本地资源的代码，清除注入的标签属性，以及清除失效的 script 、 link 标签 */</script>
+    <link root rel="stylesheet" backup="./backup/element-ui@2.15.6/index.css" onerror="loadCSS(this)" href="https://unpkg.zhimg.com/element-ui@2.15.6/lib/theme-chalk/index.css">
+    <script root type="text/javascript" crossorigin="anonymous" onerror="this.remove();" src="https://unpkg.zhimg.com/vue@2.6.10/dist/vue.min.js"></script>
+    <script backup>window.Vue || document.write(unescape('%3Cscript%20type%3D%22text/javascript%22%20crossorigin%3D%22anonymous%20src%3D%22./backup/vue@2.6.10/dist/vue.min.js%22%3E%3C/script%3E'));</script>
     <!-- 剩余省略 -->
   </head>
   <body>
-    <!-- 以下代码被提取，当 head 的所有注入的 script 加载完毕时后再写入 body 代码 -->
-    <!-- 
       <div id="app"></div>
       ...
-      <script type="text/javascript" crossorigin="anonymous" backup="./backup/moment@2.29.1/moment.min.js" onload="loadSuccess.call(this)" onerror="loadScript(this)" src="https://unpkg.zhimg.com/moment@2.29.1/min/moment.min.js" final></script>
-    -->
+      <!-- 以下是注入的 final: true 的代码 -->
+      <script root type="text/javascript" crossorigin="anonymous" onerror="this.remove();" src="https://unpkg.zhimg.com/moment@2.29.1/min/moment.min.js"></script>
+      <script backup>window.moment || document.write(unescape('%3Cscript%20type%3D%22text/javascript%22%20crossorigin%3D%22anonymous%20src%3D%22./backup/vue@2.6.10/dist/vue.min.js%22%3E%3C/script%3E'));</script>
   </body>
 </html>
 ```
@@ -128,7 +106,8 @@ if (!window.Vuex) Vue.use(VueRouter);
   </head>
   <body>
     <div id="app">
-      <!-- el 挂载的节点，render 函数渲染后的内容，下面是 final: true 的 script -->
+      ...
+      <!-- 以下是成功加载的 final: true 的代码 -->
       <script type="text/javascript" crossorigin="anonymous" src="https://unpkg.zhimg.com/moment@2.29.1/min/moment.min.js"></script>
     </div>
   </body>
@@ -149,8 +128,9 @@ CDN 失效后自动切换本地资源
   </head>
   <body>
     <div id="app">
-      <!-- el 挂载的节点，render 函数渲染后的内容，下面是 final: true 的 script -->
-      <script type="text/javascript" crossorigin="anonymous" src="./backup/moment@2.29.1/moment.min.js"></script>
+      ...
+      <!-- 以下是成功加载的 final: true 的代码 -->
+      <script type="text/javascript" crossorigin="anonymous" src="./backup/moment@2.29.1/min/moment.min.js"></script>
     </div>
   </body>
 </html>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const fs = require('fs');
 const request = require('request');
+const  { RawSource }  = require('webpack-sources');
 
 class AutoWebpackCdnPlugin {
   constructor(options) {
@@ -12,96 +12,96 @@ class AutoWebpackCdnPlugin {
     // 忽略打包已替换 cdn 的依赖库
     let externals = compiler.options.externals || {};
     this.options.modules.forEach(item => {
-      if (getVersionInNodeModules(item.from) && item.to) externals[item.from] = item.to;
+      if (this.getVersionInNodeModules(item.from) && item.to) externals[item.from] = item.to;
     });
     compiler.options.externals = externals;
 
-    // 打包后完成后，如果开启了备份，则下载 cdn 资源到 backup 目录
-    compiler.hooks.done.tap('auto-webpack-cdn-plugin', stats => {
-      if ((this.options.backup && process.env.NODE_ENV === 'production')) {
-        let filePath = stats.compilation.options.output.path;
-        if (fs.existsSync(filePath)) {
-          filePath += '/backup';
-          this.options.modules.forEach(item => {
-            let dir = `${filePath}/${item.from}@${getVersionInNodeModules(item.from)}`;
-            mkdirsSync(dir);
-            let filename = `${dir}/${getFileName(item.path)}`;
-            if (!fs.existsSync(filename)) {
-              let stream = fs.createWriteStream(filename);
-              request(item.path).pipe(stream);
-            }
-          });
-        }
-      }
+    // 输出 asset 钩子
+    compiler.hooks.emit.tapAsync('auto-webpack-cdn-plugin', (compilation, callback) => {
+      this.start(compilation, this.options, callback);
     });
+  }
 
+  start(compilation, options, callback) {
     // 注入 cdn 到 html 模板
-    compiler.hooks.emit.tap('auto-webpack-cdn-plugin', compilation => {
-      for (const name in compilation.assets) {
-        if (name === this.options.template) {
-          let contents = compilation.assets[name].source();
-          let temp = '';
-          let tempf = '';
-          let initScript = `<script type=\"text/javascript\" cdn>var totalScript=#totalScript#;var finallyScript=#finallyScript#;function loadCSS(e){let new_s=document.createElement('link');new_s.rel='stylesheet';new_s.href=e.getAttribute('backup');document.head.insertBefore(new_s,e);e.remove()}function loadScript(e){let new_s=document.createElement('script');new_s.src=e.getAttribute('backup');e.hasAttribute('crossorigin')&&new_s.setAttribute(e.getAttribute('crossorigin'));document.head.insertBefore(new_s,e);new_s.onload=loadSuccess.call(e);e.remove()}let loadSuccess=(function(){let loadedScript=0;let finalScript=0;return function(){this.getAttribute('onload')&&this.removeAttribute('onload');this.getAttribute('onerror')&&this.removeAttribute('onerror');this.getAttribute('backup')&&this.removeAttribute('backup');if(this.tagName!=='SCRIPT')return;!this.hasAttribute('final')&&loadedScript++;if(this.hasAttribute('final')){finalScript++;this.removeAttribute('final')}if(loadedScript==totalScript){document.open();document.write('#bodyContent#');document.close()}if(loadedScript===totalScript&&finalScript===finallyScript){loadedScript=null;finalScript=null;let scripts=[...document.getElementsByTagName('script')];scripts.forEach(item=>{if(item.hasAttribute('cdn'))item.remove()})}}})();</script>`;
-          let bodyContent = contents.substring(contents.indexOf('<body>') + 6, contents.indexOf('</body>'));
-          let totalScript = this.options.modules.filter(item => getType(item.path) === 'js' && !item.final).length;
-          let finallyScript = this.options.modules.filter(item => getType(item.path) === 'js' && item.final).length;
-          initScript = initScript.replace('#bodyContent#', bodyContent).replace('#totalScript#', totalScript).replace('#finallyScript#', finallyScript);
-          this.options.modules.sort((a, b) => {
-            if (getType(a.path) === 'css' && getType(b.path) === 'js') return -1;
-          });
-          this.options.modules.forEach(item => {
-            let filename = './backup/' + getFileName(item.path);
-            let url = this.options.cdnUrl;
-            url = url.replace(':name', item.from).replace(':version', getVersionInNodeModules(item.from));
-            if (item.path.substring(0, 3) !== 'http') item.path = `${url}/${item.path}`;
-            if (getType(filename) === 'js' && !item.final) temp += `<script type="text/javascript" ${this.options.crossOrigin ? `crossorigin="${this.options.crossOrigin}"` : ''}`
-              + ` ${this.options.backup ? `backup="${filename}"` : ''} onload="loadSuccess.call(this)" onerror="loadScript(this)" src="${item.path}"></script>`;
-            if (getType(filename) === 'css') temp += `<link rel="stylesheet" ${this.options.backup ? `backup="${filename}"` : ''} onload="loadSuccess.call(this)" onerror="loadCSS(this)" href="${item.path}"></link>`;
-            if (getType(filename) === 'js' && item.final) tempf += `<script type="text/javascript" ${this.options.crossOrigin ? `crossorigin="${this.options.crossOrigin}"` : ''}`
-              + ` ${this.options.backup ? `backup="${filename}"` : ''} onload="loadSuccess.call(this)" onerror="loadScript(this)" src="${item.path}" final></script>`;
-          });
-          let withoutComments = contents.replace('</head>', initScript + temp + '</head>');
-          withoutComments = withoutComments.replace(bodyContent, '').replace('</body>', tempf + '</body>');
-          compilation.assets[name] = {
-            source: () => withoutComments,
-            size: () => withoutComments.length
-          };
-          break;
-        }
-      }
-    });
-
-    // 取完整文件名
-    function getFileName(o) {
-      let pos = o.lastIndexOf('/');
-      return o.substring(pos + 1);
-    }
-
-    // 获取文件后缀
-    function getType(file) {
-      return file.replace(/.+\./, '');
-    }
-
-    // 取模块版本
-    function getVersionInNodeModules(name, pathToNodeModules = process.cwd()) {
-      try {
-        return require(path.join(pathToNodeModules, 'node_modules', name, 'package.json')).version;
-      } catch (e) {
-        return null;
+    for (const name in compilation.assets) {
+      if (name === options.template) {
+        let contents = compilation.assets[name].source();
+        let temp = '';
+        let tempf = '';
+        let initScript = `<script type="text/javascript" cdn>function loadCSS(e){let new_s=document.createElement('link');new_s.rel='stylesheet';new_s.href=e.getAttribute('backup');document.head.insertBefore(new_s,e);e.remove()}window.onload=()=>{let scripts=[...document.getElementsByTagName('script')];scripts.forEach(item=>{if(item.hasAttribute('cdn'))item.remove();if(item.hasAttribute('backup'))item.remove();if(item.hasAttribute('root')){item.removeAttribute('root');item.removeAttribute('onerror')}});let links=[...document.getElementsByTagName('link')];links.forEach(item=>{if(item.hasAttribute('backup'))item.remove();if(item.hasAttribute('root')){item.removeAttribute('root');item.removeAttribute('onerror')}})};</script>`;
+        options.modules.sort((a, b) => {
+          if (this.getType(a.path) === 'css' && this.getType(b.path) === 'js') return -1;
+        });
+        options.modules.forEach(item => {
+          let filename = `./backup/${item.from}@${this.getVersionInNodeModules(item.from)}/${item.path}`;
+          let url = options.cdnUrl;
+          url = url.replace(':name', item.from).replace(':version', this.getVersionInNodeModules(item.from));
+          if (item.path.substring(0, 3) !== 'http') item.netpath = `${url}/${item.path}`;
+          if (this.getType(filename) === 'js' && !item.final) {
+            temp += `<script root type="text/javascript" ${options.crossOrigin ? `crossorigin="${options.crossOrigin}"` : ''}`
+              + ` onerror="this.remove();" src="${item.netpath}"></script>`;
+            options.backup && (temp += `<script backup>window.${item.to} || document.write(unescape('%3Cscript%20type%3D%22text/javascript%22%20${options.crossOrigin ? `crossorigin%253D"${options.crossOrigin}"%20` : ''}src%3D%22${filename}%22%3E%3C/script%3E'));</script>`);
+          }
+          if (this.getType(filename) === 'css') temp += `<link rel="stylesheet" ${options.backup ? `backup="${filename}"` : ''} href="${item.netpath}" onerror="loadCSS(this)"></link>`;
+          if (this.getType(filename) === 'js' && item.final) {
+            tempf += `<script root type="text/javascript" ${options.crossOrigin ? `crossorigin="${options.crossOrigin}"` : ''}`
+              + ` onerror="this.remove();" src="${item.netpath}"></script>`;
+            options.backup && (tempf += `<script backup>window.${item.to} || document.write(unescape('%3Cscript%20type%3D%22text/javascript%22%20${options.crossOrigin ? `crossorigin%3D%22"${options.crossOrigin}"%20` : ''}src%3D%22${filename}%22%3E%3C/script%3E'));</script>`);
+          }
+        });
+        let withoutComments = contents.replace('</head>', initScript + temp + '</head>');
+        withoutComments = withoutComments.replace('</body>', tempf + '</body>');
+        compilation.assets[name] = new RawSource(withoutComments);
+        break;
       }
     }
 
-    // 递归创建目录 同步方法
-    function mkdirsSync(dirname) {
-      if (fs.existsSync(dirname)) {
-        return true;
-      } else {
-        if (mkdirsSync(path.dirname(dirname))) {
-          fs.mkdirSync(dirname);
-          return true;
-        }
+    // 如果开启了备份，则下载 cdn 资源到 backup 目录
+    let n = 0;
+    if (options.backup) {
+      let filePath = 'backup';
+      for (let i = 0; i < options.modules.length; i++) {
+        let item = options.modules[i];
+        let dir = `${filePath}/${item.from}@${this.getVersionInNodeModules(item.from)}/${this.getPath(item.path)}`;
+        let filename = `${dir}/${this.getFileName(item.path)}`;
+        request(item.netpath, (error, response, body) => {
+          n++;
+          if (error) throw error;
+          if (response.statusCode === 200) {
+            compilation.assets[filename] = new RawSource(body);
+            n === options.modules.length && callback();
+          } else {
+            throw `下载 ${item.netpath} 文件失败！`;
+          }
+        });
       }
+    }
+  }
+
+  // 取完整文件名
+  getFileName(o) {
+    let pos = o.lastIndexOf('/');
+    return o.substring(pos + 1);
+  }
+
+  // 获取文件后缀
+  getType(file) {
+    return file.replace(/.+\./, '');
+  }
+
+  // 获取路径
+  getPath(o) {
+    let pos = o.lastIndexOf('/');
+    return o.substring(0, o.length - (o.length - pos));
+  }
+
+  // 取模块版本
+  getVersionInNodeModules(name, pathToNodeModules = process.cwd()) {
+    try {
+      return require(path.join(pathToNodeModules, 'node_modules', name, 'package.json')).version;
+    } catch (e) {
+      return null;
     }
   }
 }
